7f7a93e9678a787f344fb7d3f607f5e608b7fb29
Refs <lhx1010339808@outlook.com>
2018-05-21 15:22:08 +0800

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CreateComponent } from './pages/create/create.component';
+import { CompleteComponent } from './pages/complete/complete.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CreateComponent,
+    CompleteComponent
   ],
   imports: [
     BrowserModule
