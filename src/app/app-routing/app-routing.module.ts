751a3f77a61fd325adcc7454984ad0dfcf4e8f21
Refs <lhx1010339808@outlook.com>
2018-05-21 16:06:13 +0800

    .

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,9 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { CompleteComponent } from '../pages/complete/complete.component';
+import { CreateComponent } from '../pages/create/create.component';
+import { HomeComponent } from '../pages/home/home.component';
+
+
+const appRoutes: Routes = [
+  { path: '',  component: HomeComponent },
+  { path: 'create',  component: CreateComponent },
+  { path: 'complete',  component: CompleteComponent },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
+
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
+  ],
+  exports: [
+    RouterModule
   ],
   declarations: []
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
+
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -16,7 +19,8 @@ import { CompleteComponent } from './pages/complete/complete.component';
     CompleteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
